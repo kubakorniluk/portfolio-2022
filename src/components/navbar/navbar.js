af09b834b73388b7add922e3c8007a4103995004
Kuba Korniluk <kornilukkuba@gmail.com>
2020-08-22 04:00:14 +0200

    working on navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './logo/logo.js'
+const Navbar = () => {
+    return (  
+        <header className='navbar'>
+            <Logo />
+        </header>
+    );
+}
+ 
+export default Navbar;
