e0206ead9e8a2993fc676e6aa57fd0c4482364e0
Kuba Korniluk <kornilukkuba@gmail.com>
2020-08-24 03:29:47 +0200

    adjusted margins and still working on sidebar

--- src/components/header/components/sidebar/toggle/toggle.jsx
+++ src/components/header/components/sidebar/toggle/toggle.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './toggle.scss';
-
-const ToggleSidebar = () => {
-    return (
-        <a className='sidebar__toggle'>
-            <span className='sidebar__toggle--bar'></span>
-            <span className='sidebar__toggle--bar'></span>
-            <span className='sidebar__toggle--bar'></span>
-        </a>
-    );
+class ToggleSidebar extends React.Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            clicked: false
+        }
+    }
+    handleClick() {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+    render() {
+        return (
+            <i
+                onClick={this.handleClick} 
+                className={this.state.clicked ? 'sidebar__toggle sidebar__toggle--active' : 'sidebar__toggle'}
+            >
+                <span className='sidebar__toggle--bar'></span>
+                <span className='sidebar__toggle--bar'></span>
+                <span className='sidebar__toggle--bar'></span>
+            </i>
+        );
+    }
 }
  
 export default ToggleSidebar;
