686c94ed04c9f53a4aa46c92c6d08ea92c145cba
Kuba Korniluk <kornilukkuba@gmail.com>
2020-08-24 03:27:25 +0200

    added icon component based on font awesome

---
+++ src/components/icon/icon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons';
+import './icon.scss'
+library.add(faFacebook, faGithub, faCodepen);
+const Icon = (props) => {
+    return (
+        <i className='icon'>
+            <FontAwesomeIcon icon={props.name} />
+        </i>
+    );
+}
+ 
+export default Icon;
