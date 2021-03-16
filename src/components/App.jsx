ebe050aea22e2e6da8f63105931415ffc6281a04
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-16 23:11:10 +0100

    added global styles to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalStyles from '../styles/globalStyles';
 import Homepage from '../routes/Homepage';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/">
-                    <Homepage />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Homepage />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </>
     );
 }
 export default App;
