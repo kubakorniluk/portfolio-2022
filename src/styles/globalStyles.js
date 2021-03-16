a706ac78df87bb4d1a9a92cdb7e5e80840cd3df0
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-16 23:01:43 +0100

    added global styling

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+        user-select: none;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        display: block;
+        margin: 0;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    a:link {
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/homepageStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const HomepageStyles = createGlobalStyle`
+    body {
+        background-color: #090909;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden;
+    }
+`;
+
+export default HomepageStyles;
