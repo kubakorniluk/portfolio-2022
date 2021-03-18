2a422b5a4bec7e8c0c3362360da74047aae17e78
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-18 18:55:53 +0100

    Added Footer to Homepage route

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomepageStyles from '../styles/homepageStyles';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Homepage = () => {
     return (
         <>
             <HomepageStyles />
             <Header />
+            <Footer />
         </>
     );
 }
