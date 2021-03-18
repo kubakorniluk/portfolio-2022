40332357d36d406fdcbc00f9fc85faf0360c4b46
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-18 18:55:19 +0100

    Improved responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import Logo from './Logo';
+import SidebarToggle from './SidebarToggle';
 
 const StyledHeader = styled.header`
     display: flex;
@@ -8,15 +9,21 @@ const StyledHeader = styled.header`
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    padding: 6em;
-    @media only screen and (min-width: 0px) and (max-width: 576.99px) {
-        padding: 2.5em;
+    padding: 5em;
+    @media only screen and (min-width: 0px) and (max-width: 320.99px) {
+        padding: 1em;
+    }
+    @media only screen and (min-width: 321px) and (max-width: 576.99px) {
+        padding: 1.5em;
     }
     @media only screen and (min-width: 577px) and (max-width: 768.99px) {
-        padding: 4.5em;
+        padding: 3em;
+    }
+    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
+        padding: 3.5em;
     }
     @media only screen and (min-width: 1025px) and (max-width: 1366.99px) { 
-        padding: 5em;
+        padding: 3.5em;
     }
 `;
 
@@ -24,6 +31,7 @@ const Header = () => {
     return ( 
         <StyledHeader>
             <Logo />
+            <SidebarToggle />
         </StyledHeader>
     );
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,29 +8,32 @@ const StyledLogo = styled.h1`
     color: #ffffff;
     font-family: 'Quicksand', sans-serif;
     font-weight: normal;
-    font-size: 110%;
+    font-size: 105%;
+    padding: 1em;
+    cursor: pointer;
     &::after {
         content: '';
         display: block;   
-        margin-top: 0.75em;
+        margin-top: .75em;
         width: 25%;
-        height: 0.1em;
+        height: .1em;
         background-color: #ffffff;
-        transition: 0.75s ease;
-        cursor: pointer;
-
+        transition: .75s ease;
     }
     &:hover::after {
         width: 100%;
     } 
-    a {
-        color: #ffffff;
-    }
-    @media only screen and (min-width: 0px) and (max-width: 576.99px) {
+    @media only screen and (min-width: 0px) and (max-width: 320.99px) {
         font-size: 90%;
     }
-    @media only screen and (min-width: 769px) and(max-width: 1024.99px) { 
-        font-size: 120%;
+    @media only screen and (min-width: 321px) and (max-width: 576.99px) {
+        font-size: 97.5%;
+    }
+    @media only screen and (min-width: 577px) and (max-width: 768.99px) {
+        font-size: 105%;
+    }
+    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
+        font-size: 110%;
     }
 `;
 
