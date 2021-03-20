8af549f489e2fd29debb879a82ecb8052f1918ae
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-20 16:32:06 +0100

    Added CursorTracker to Homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import HomepageStyles from '../styles/homepageStyles';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import CursorTracker from '../components/CursorTracker';
 
 const Homepage = () => {
     return (
         <>
+            <CursorTracker />
             <HomepageStyles />
             <Header />
             <Footer />
