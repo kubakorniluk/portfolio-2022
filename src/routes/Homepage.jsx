443150a0abb47f5521077a6c3e97fdf1866f7353
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-27 09:54:16 +0100

    Resize event for cursor disabling on mobile devices

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomepageStyles from '../styles/homepageStyles';
 import Header from '../components/Header';
+import Main from '../components/Main';
 import Footer from '../components/Footer';
-import CursorTracker from '../components/CursorTracker';
+import Cursor from '../components/CursorTracker';
+import ContextProvider from '../components/ContextProvider';
 
 const Homepage = () => {
+    const [ screenWidth, setScreenWidth ] = useState(window.innerWidth);
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    })
+    const handleResize = () => setScreenWidth(window.innerWidth);
     return (
-        <>
-            <CursorTracker />
+        <ContextProvider>
+            { (screenWidth >= 1025) ? <Cursor /> : null }
             <HomepageStyles />
             <Header />
+            <Main />
             <Footer />
-        </>
+        </ContextProvider>
     );
 }
  
