d7c40375d5466b1c2643c752ac041bd5b67cf6a8
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-28 20:58:03 +0200

    Moved global styling to this component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,26 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalStyles from '../styles/globalStyles';
+import { createGlobalStyle } from 'styled-components';
+import ProjectContextProvider from './ProjectContextProvider';
+
+const GlobalStyles = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+        user-select: none;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+        cursor: none;
+    }
+    body {
+        margin: 0;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    a:link {
+        text-decoration: none;
+    }
+`;
 
 const App = () => {
     const Homepage = lazy(() => import('../routes/Homepage'));
@@ -10,11 +30,14 @@ const App = () => {
             <BrowserRouter>
                 <Suspense fallback="Loading...">
                     <Switch>
-                        <Route exact path="/" component={Homepage}/>
+                        <ProjectContextProvider>
+                            <Route exact path="/" component={Homepage}/>
+                        </ProjectContextProvider>
                     </Switch>   
                 </Suspense>
             </BrowserRouter>
         </>
     );
 }
+
 export default App;
