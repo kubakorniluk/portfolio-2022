ea7d89a8e10e0947dfaf5b5e91ede7e456de3a96
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-28 20:56:53 +0200

    Added context for changing background-image & moved global styling to this component

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,10 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import HomepageStyles from '../styles/homepageStyles';
+import React, { useState, useEffect, useContext } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
-import Cursor from '../components/CursorTracker';
-import ContextProvider from '../components/ContextProvider';
+import Cursor from '../components/Cursor';
+import { ProjectContext } from '../components/ProjectContextProvider';
+
+const HomepageStyles = createGlobalStyle`
+    body {
+        display: block;
+        background-image: ${props => `url(${props.img})`};
+        width: 100vw;
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        overflow: hidden;
+        transition: 1.05s;
+    }
+    a {
+        color: #ffffff;
+    }
+    #root {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .825);
+        backdrop-filter: blur(5px) grayscale(1);
+    }
+`;
 
 const Homepage = () => {
     const [ screenWidth, setScreenWidth ] = useState(window.innerWidth);
@@ -15,14 +41,21 @@ const Homepage = () => {
         };
     })
     const handleResize = () => setScreenWidth(window.innerWidth);
+
+    const { projectsData, currentProject } = useContext(ProjectContext);
+    const currentImage = projectsData.map(i => i.img)[currentProject];
     return (
-        <ContextProvider>
-            { (screenWidth >= 1025) ? <Cursor /> : null }
-            <HomepageStyles />
+        <>      
+            { (screenWidth > 1024) ? <Cursor /> : null }
+            <HomepageStyles img={
+                (currentImage !== undefined) ? 
+                require(`../${currentImage}`) : 
+                null
+            }/>
             <Header />
             <Main />
             <Footer />
-        </ContextProvider>
+        </>
     );
 }
  
