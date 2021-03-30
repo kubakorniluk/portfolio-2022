54060e26bbd8eb45a522731c398b1a03ec46b101
Kuba Korniluk <kornilukkuba@gmail.com>
2021-03-30 22:17:47 +0200

    Main content is vertically centered now

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,8 @@ import ProjectBrowser from './ProjectBrowser';
 const StyledMain = styled.main`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
+    justify-content: center;
     width: 100%;
     height: 100%;
     padding: 0 6em;
