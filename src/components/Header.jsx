6824fd0a666a74851daa1cde6254ed6135c38602
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-16 19:33:44 +0200

    Added prop-types & changed media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo from './Logo';
-import Sidebar from './Sidebar';
+import PropTypes from 'prop-types';
 
 const StyledHeader = styled.header`
     display: flex;
@@ -9,31 +8,24 @@ const StyledHeader = styled.header`
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    padding: 5em;
-    @media only screen and (min-width: 0px) and (max-width: 320.99px) {
-        padding: 1em;
-    }
-    @media only screen and (min-width: 321px) and (max-width: 576.99px) {
-        padding: 1.5em;
-    }
-    @media only screen and (min-width: 577px) and (max-width: 768.99px) {
-        padding: 3em;
-    }
-    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
-        padding: 3.5em;
-    }
-    @media only screen and (min-width: 1025px) and (max-width: 1366.99px) { 
-        padding: 3.5em;
-    }
+    padding: 5em 6.5em;
+    @media only screen and (min-width: 0px) and (max-width: 320.99px) { padding: 1em; }
+    @media only screen and (min-width: 321px) and (max-width: 576.99px) { padding: 1.5em; }
+    @media only screen and (min-width: 577px) and (max-width: 768.99px) { padding: 3em; }
+    @media only screen and (min-width: 769px) and (max-width: 1024.99px) {  padding: 3.5em; }
+    @media only screen and (min-width: 1025px) and (max-width: 1366.99px) { padding: 2.5em 4em; }
 `;
 
-const Header = () => {
+const Header = ({ children }) => {
     return ( 
         <StyledHeader>
-            <Logo />
-            <Sidebar />
+            { children }
         </StyledHeader>
     );
 }
  
 export default Header;
+
+Header.propTypes = {
+    children: PropTypes.any.isRequired
+}
