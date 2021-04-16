cd810067c05550a987aae318fde408f0872bb73c
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-16 19:37:44 +0200

    Added function for handling ProjectCounter & prop-types

--- src/components/ProjectContextProvider.jsx
+++ src/components/ProjectContextProvider.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useMemo, createContext } from 'react';
+import React, { 
+    useState, 
+    useEffect, 
+    useMemo, 
+    createContext 
+} from 'react';
+import PropTypes from 'prop-types';
 
 export const ProjectContext = createContext();
 
@@ -8,12 +14,8 @@ const ProjectContextProvider = ({
     const [ projectsData, setProjectsData ] = useState([]);
     const [ currentProject, setCurrentProject ] = useState(0);
     useEffect(() => {
-        (
-            async () => {
-                const data = await import('../data/projects.json');
-                setProjectsData(data.default);
-            }
-        )();
+        import('../data/projects.json')
+        .then(data => setProjectsData(data.default));
     });
     const handleMouseWheel = (event) => {
         if(event.deltaY < 0) { 
@@ -27,11 +29,24 @@ const ProjectContextProvider = ({
             setCurrentProject(currentProject - 1)
         }
     }
+    const handleProjectCounter = (action) => {
+        if (action === 'NEXT') {
+            (currentProject + 1 > projectsData.length - 1) ? 
+            setCurrentProject(0) : 
+            setCurrentProject(currentProject + 1)
+        } else if(action === 'PREVIOUS') {
+            (currentProject - 1 < 0) ? 
+            setCurrentProject(projectsData.length - 1) : 
+            setCurrentProject(currentProject - 1)
+        }
+    } 
     const memoizedData = useMemo(() => projectsData, [projectsData]);
+    const memoizedProject = useMemo(() => currentProject, [currentProject]);
     const value = {
         projectsData: memoizedData,
-        currentProject,
-        handleMouseWheel
+        currentProject: memoizedProject,
+        handleMouseWheel,
+        handleProjectCounter
     };
     return (
         <ProjectContext.Provider value={value}>
@@ -41,3 +56,7 @@ const ProjectContextProvider = ({
 }
 
 export default ProjectContextProvider;
+
+ProjectContextProvider.propTypes = {
+    children: PropTypes.any.isRequired
+}
