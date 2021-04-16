531317252bdb08b9936faee343a091d50fb19f21
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-16 19:30:09 +0200

    Added resize event for displaying cursor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
+import { 
+    BrowserRouter, 
+    Route, 
+    Switch 
+} from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
+import Cursor from './Cursor';
 import ProjectContextProvider from './ProjectContextProvider';
+import LoadingScreen from './LoadingScreen';
 
 const GlobalStyles = createGlobalStyle`
     html {
@@ -17,22 +23,44 @@ const GlobalStyles = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
-    a:link {
+    a {
         text-decoration: none;
+        color: #ffffff;
+    }
+    *:focus {
+        outline: 0;
     }
 `;
 
 const App = () => {
-    const Homepage = lazy(() => import('../routes/Homepage'));
+    const [ screenWidth, setScreenWidth ] = useState(window.innerWidth);
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    })
+    const handleResize = () => setScreenWidth(window.innerWidth);
+
+    const Homepage = lazy(() => {
+        return new Promise(resolve => {
+            setTimeout(
+                () => resolve(import('../routes/Homepage')), 
+            1000)
+        })
+    });
     return (
         <>
+            { (screenWidth >= 1024) ? <Cursor /> : null }
             <GlobalStyles />
             <BrowserRouter>
-                <Suspense fallback="Loading...">
+                <Suspense fallback={<LoadingScreen />}>
                     <Switch>
-                        <ProjectContextProvider>
-                            <Route exact path="/" component={Homepage}/>
-                        </ProjectContextProvider>
+                        <Route exact path="/">
+                            <ProjectContextProvider>
+                                <Homepage />
+                            </ProjectContextProvider>
+                        </Route>
                     </Switch>   
                 </Suspense>
             </BrowserRouter>
