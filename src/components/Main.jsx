96397b15906722f2743d7d712c63e0822701b97d
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-16 19:34:09 +0200

    Added prop-types & changed media queries

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ProjectBrowser from './ProjectBrowser';
+import PropTypes from 'prop-types';
 
 const StyledMain = styled.main`
     display: flex;
@@ -9,30 +9,24 @@ const StyledMain = styled.main`
     justify-content: center;
     width: 100%;
     height: 100%;
-    padding: 0 6em;
-    @media only screen and (min-width: 0px) and (max-width: 320.99px) {
-        padding: 0 2em;
-    }
-    @media only screen and (min-width: 321px) and (max-width: 576.99px) {
-        padding: 0 2.5em;
-    }
-    @media only screen and (min-width: 577px) and (max-width: 768.99px) {
-        padding: 0 4em;
-    }
-    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
-        padding: 0 4.5em;
-    }
-    @media only screen and (min-width: 1025px) and (max-width: 1366.99px) { 
-        padding: 0 4.5em;
-    }
+    padding: 0 7.5em;
+    @media only screen and (min-width: 0px) and (max-width: 320.99px) { padding: 0 2em; }
+    @media only screen and (min-width: 321px) and (max-width: 576.99px) { padding: 0 2.5em; }
+    @media only screen and (min-width: 577px) and (max-width: 768.99px) { padding: 0 4em; }
+    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { padding: 0 4.5em; }
+    @media only screen and (min-width: 1025px) and (max-width: 1366.99px) { padding: 0 5em; }
 `;
 
-const Main = () => {
+const Main = ({ children }) => {
     return (  
         <StyledMain>
-            <ProjectBrowser />
+            { children }
         </StyledMain>
     );
 }
  
 export default Main;
+
+Main.propTypes = {
+    children: PropTypes.any.isRequired
+}
