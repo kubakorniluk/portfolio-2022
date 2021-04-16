63ad4f8f08fa2c30a2c9a8c4798d5133ea40b0cc
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-16 19:36:57 +0200

    Added border to the ProjectBrowser

--- src/components/ProjectBrowser.jsx
+++ src/components/ProjectBrowser.jsx
@@ -1,120 +1,73 @@
 import React, { useEffect, useContext } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { ProjectContext } from './ProjectContextProvider';
-
-const slideIn = keyframes`
-    from {
-        transform: scaleY(0);
-        transform-origin: top;
-        
-    }
-    to {
-        transform: scaleY(1);
-        transform-origin: top;
-        
-    }
-`;
-const slideOut = keyframes`
-    from {
-        transform: scaleY(1);
-        transform-origin: bottom;
-        
-    }
-    to {
-        transform: scaleY(0);
-        transform-origin: bottom;
-        
-    }
-`;
+import Project from './Project';
+ 
 const StyledProjectBrowser = styled.section`
     position: relative;
     display: flex;
-    width: 50vw;
+    flex-direction: column;
+    width: 60%;
     height: 100%;
+    padding: 2em;
     span {
         position: absolute;
-        z-index: 999;
         display: block;
-        width: .5em;
-        height: .5em;
+        width: .75em;
+        height: .75em;
         transition: .1s;
     }
     .top-left {
-        border-top: .125em solid #584cea;
-        border-left: .125em solid #584cea;
-        bottom: -.5em;
-        right: -.5em;
-    }
-    .top-right {
-        border-top: .125em solid #584cea;
-        border-right: .125em solid #584cea;
-        bottom: -.5em;
-        left: -.5em;
-    }
-    .bottom-left {
-        border-bottom: .125em solid #584cea;
-        border-left: .125em solid #584cea;
-        top: -.5em;
-        right: -.5em;
-    }
-    .bottom-right {
-        border-bottom: .125em solid #584cea;
-        border-right: .125em solid #584cea;
-        top: -.5em;
-        left: -.5em;
-    }
-`;
-const ProjectImage = styled.figure`
-    margin: 0;
-    position: relative;
-    display: block;
-    width: 100%;
-    height: 100%;
-    background-image: ${props => `url('${props.img}')`};
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    .overlay {
-        position: absolute;
-        z-index: 995;
+        border-top: .1em solid #ffffff;
+        border-left: .1em solid #ffffff;
         top: 0;
         left: 0;
-        width: 100%;
-        height: 100%;
-        backdrop-filter: grayscale(1);
     }
-    .slide { 
-        position: absolute;
-        z-index: 996;
+    .top-right {
+        border-top: .1em solid #ffffff;
+        border-right: .1em solid #ffffff;
         top: 0;
         right: 0;
-        width: 100%;
-        height: 100%;
-        background-color: #ffffff;
-        animation: .35s linear forwards ${slideIn},  .35s linear .7s forwards ${slideOut};
     }
+    .bottom-left {
+        border-bottom: .1em solid #ffffff;
+        border-left: .1em solid #ffffff;
+        bottom: 0;
+        left: 0;
+    }
+    .bottom-right {
+        border-bottom: .1em solid #ffffff;
+        border-right: .1em solid #ffffff;
+        bottom: 0;
+        right: 0;
+    }
+    @media only screen and (min-width: 0px) and (max-width: 320.99px) {
+        width: 100%;
+        height: 90%;
+        padding: 1em;
+    }
+    @media only screen and (min-width: 321px) and (max-width: 576.99px) {
+        width: 100%;
+        height: 90%;
+        padding: 1.25em;
+    }
+    @media only screen and (min-width: 577px) and (max-width: 768.99px) {
+        width: 80%;
+        height: 80%;
+        padding: 1.5em;
+    }
+    @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
+        width: 70%;
+        height: 80%;
+        padding: 1.5em;
+    }
+    @media only screen and (min-width: 1025px) and (max-width: 1366.99px) {
+        padding: 1.5em;
+    }
+    
 `;
-const ProjectTitle = styled.figcaption`
-    position: absolute;
-    z-index: 996;
-    top: 50%;
-    left: 0;
-    transform: translate(-0%, -50%);
-    margin: 0;
-    font-size: 400%;
-    font-weight: 900;
-    font-family: 'Times New Roman', Times, serif;
-    color: #584cea;
-    margin-left: -10%;
-    margin-right: 90%;
-`;
-
 const ProjectBrowser = () => {
-    const { 
-        handleMouseWheel, 
-        projectsData, 
-        currentProject 
-    } = useContext(ProjectContext);
+    const { handleMouseWheel } = useContext(ProjectContext);
     useEffect(() => {
         setTimeout(
             () => {
@@ -129,26 +82,13 @@ const ProjectBrowser = () => {
             );
         }
     });
-    const projects = projectsData.map(item => {
-        const { 
-            id,
-            img,
-            title,
-            client,
-            website,
-            sourceCode
-        } = item;
-        return (
-            <ProjectImage key={id} img={require(`../${img}`)}>
-                <div className="slide"></div>
-                <div className="overlay"></div>
-                <ProjectTitle>{title}</ProjectTitle>
-            </ProjectImage>
-        )
-    });
     return ( 
         <StyledProjectBrowser>
-            { projects[currentProject] }
+            <span className="top-left"></span>
+            <span className="top-right"></span>
+            <span className="bottom-left"></span>
+            <span className="bottom-right"></span>
+            <Project />
         </StyledProjectBrowser>
     );
 }
