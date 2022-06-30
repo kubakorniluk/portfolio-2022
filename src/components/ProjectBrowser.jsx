3d54d25ed2079c3b0e05a8c5e2cf48cd447126d6
Kuba Korniluk <kornilukkuba@gmail.com>
2022-06-30 20:10:10 +0200

    Deleted corner borders

--- src/components/ProjectBrowser.jsx
+++ src/components/ProjectBrowser.jsx
@@ -9,60 +9,23 @@ const StyledProjectBrowser = styled.section`
     flex-direction: column;
     width: 60%;
     height: 100%;
-    padding: 2em;
-    span {
-        position: absolute;
-        display: block;
-        width: .75em;
-        height: .75em;
-        transition: .1s;
-    }
-    .top-left {
-        border-top: .1em solid #ffffff;
-        border-left: .1em solid #ffffff;
-        top: 0;
-        left: 0;
-    }
-    .top-right {
-        border-top: .1em solid #ffffff;
-        border-right: .1em solid #ffffff;
-        top: 0;
-        right: 0;
-    }
-    .bottom-left {
-        border-bottom: .1em solid #ffffff;
-        border-left: .1em solid #ffffff;
-        bottom: 0;
-        left: 0;
-    }
-    .bottom-right {
-        border-bottom: .1em solid #ffffff;
-        border-right: .1em solid #ffffff;
-        bottom: 0;
-        right: 0;
-    }
     @media only screen and (min-width: 0px) and (max-width: 320.99px) {
         width: 100%;
         height: 90%;
-        padding: 1em;
     }
     @media only screen and (min-width: 321px) and (max-width: 576.99px) {
         width: 100%;
         height: 90%;
-        padding: 1.25em;
     }
     @media only screen and (min-width: 577px) and (max-width: 768.99px) {
         width: 80%;
         height: 80%;
-        padding: 1.5em;
     }
     @media only screen and (min-width: 769px) and (max-width: 1024.99px) { 
         width: 70%;
         height: 80%;
-        padding: 1.5em;
     }
     @media only screen and (min-width: 1025px) and (max-width: 1366.99px) {
-        padding: 1.5em;
     }
     
 `;
@@ -84,10 +47,6 @@ const ProjectBrowser = () => {
     });
     return ( 
         <StyledProjectBrowser>
-            <span className="top-left"></span>
-            <span className="top-right"></span>
-            <span className="bottom-left"></span>
-            <span className="bottom-right"></span>
             <Project />
         </StyledProjectBrowser>
     );
